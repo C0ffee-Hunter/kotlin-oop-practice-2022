8a5977ea1dcf2de1940226db341847c866a0e873
C0ffee-Hunter <59738833+C0ffee-Hunter@users.noreply.github.com>
2022-05-04 03:45:54 +0300

    Update code

--- src/main/kotlin/lab4/controller.kt
+++ src/main/kotlin/lab4/controller.kt
@@ -1,4 +1,36 @@
-package main.kotlin.lab4
+package lab4.controller
 
-class controller {
+import main.kotlin.lab4.GAME_NOT_FINISHED
+import main.kotlin.lab4.State
+import main.kotlin.lab4.Move
+import main.kotlin.lab4.Model
+
+class Controller(private val model: Model) {
+    init {
+        startGame()
+    }
+
+    private fun startGame() {
+        while (model.state in GAME_NOT_FINISHED) {
+            val input = readln()
+            var movent: Move = Move.Wait
+            for (i in input) {
+                when (i) {
+                    'w' -> movent = Move.Up
+                    'a' -> movent = Move.Left
+                    's' -> movent = Move.Down
+                    'd' -> movent = Move.Right
+                    'q' -> movent = Move.EXIT
+                    '1' -> movent = State.Save
+                }
+                try {
+                    model.doMove(state)
+                } catch (e: Exception) {
+                    println(e.message)
+                }
+            }
+            if(state == State.EXIT)
+                break ///Грубый метод
+        }
+    }
 }
