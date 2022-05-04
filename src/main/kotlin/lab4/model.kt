0cd2773fa683577f4d23a1be062d76d90661e578
C0ffee-Hunter <59738833+C0ffee-Hunter@users.noreply.github.com>
2022-05-04 03:46:41 +0300

    Update code

--- src/main/kotlin/lab4/model.kt
+++ src/main/kotlin/lab4/model.kt
@@ -9,7 +9,7 @@ enum class Cells(private val textValue: String)
     PLAYER("P"),
     FINISH("E");
 
-    override fun toString(): String = textValue;
+    override fun toString(): String = textValue
 }
 
 enum class State(private val textValue: String) {
@@ -21,6 +21,7 @@ enum class State(private val textValue: String) {
 
 enum class Move(private val textValue: String)
 {
+    Wait("wait"),
     Up("up"),
     Down("down"),
     Left("left"),
@@ -29,9 +30,12 @@ enum class Move(private val textValue: String)
     override fun toString(): String = textValue;
 }
 
+val GAME_NOT_FINISHED = setOf(Move.Up, Move.Down, Move.Left, Move.Right, State.WAIT_MOVE)
+
 fun readMaze(): Map<Pair<Int, Int>, Char>
 {
-    val input = File("C:\\Users\\rogin\\Рабочий стол\\kotlin\\src\\main\\kotlin\\lab4\\maze.txt").readLines().withIndex().flatMap { indexedValue ->
+    val level = File("C:\\Users\\Даниил\\Desktop\\kotlin-oop-practice-2022-master (1)\\kotlin-oop-practice-2022-master\\" +
+            "src\\main\\kotlin\\lab4\\maze.txt").readLines().withIndex().flatMap { indexedValue ->
                 val xCord = indexedValue.index
                 indexedValue.value.toCharArray().withIndex().map { indexedChar ->
                     val yCord = indexedChar.index
@@ -39,16 +43,51 @@ fun readMaze(): Map<Pair<Int, Int>, Char>
                 }
             }
             .toMap()
-        return input
+        return level
 }
 
-class model
-{
-    val field = readMaze().toMutableMap()
-    var state: State = State.WAIT_MOVE
+/*val level = File("C:\\Users\\Даниил\\Desktop\\kotlin-oop-practice-2022-master (1)\\kotlin-oop-practice-2022-master\\" +
+        "src\\main\\kotlin\\lab4\\maze.txt").readLines()
+val LEVEL_COL = level[0].length
+val LEVEL_ROW = level.size*/
 
-    fun doMove(move: Move)
+class Model
+{
+    //private val _board: MutableList<MutableList<Cells>> = mutableListOf()//initEmptyBoard()
+    private var row = 0
+    private var col = 0
+    //private val board: List<List<Cells>>
+        //get() = _board
+    val field = readMaze().toMutableMap()
+    var move: Move = Move.Wait
+
+    fun doMove(move: Move) {
+        var playerRow = row
+        var playerCol = col
+
+        when (move) {
+            Move.Right -> playerCol++
+            Move.Left -> playerCol--
+            Move.Up -> playerRow--
+            Move.Down -> playerRow++
+            //else -> require(move != State.EXIT) { "Game finished" }
+        }
+
+        require(_board[playerRow][playerCol] != Cells.WALL) { "Wrong move" }
+
+        val player = _board[playerRow][playerCol]
+
+        _board[row][col] = Cells.EMPTY
+        _board[playerRow][playerCol] = Cells.PLAYER
+
+        require(Cells.PLAYER != Cells.FINISH) { "Congratulations. Game finish" }
+
+    }
+
+    fun saveGame(move: Move)
     {
 
     }
+
+
 }
